d6ce7a3c91ac95a56c641114834d8de82bb73bd8
Ilya Isaev <iisaev@okko.tv>
2021-03-21 14:17:35 +0300

    opening zip files

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"archive/zip"
+	"flag"
+	"fmt"
+	"log"
+)
 
 func main() {
-    fmt.Println("Hello, World!")
+	input := flag.String("input", "notes.zip", "Archive file, exported from simplenote.com")
+	output := flag.String("output", ".", "Output folder (GitJournal repo)")
+	flag.Parse()
+
+	zipReader, err := zip.OpenReader(*input)
+	if err != nil {
+		fmt.Println("Unable to open ", *input)
+		log.Fatal(err)
+	}
+	fmt.Println("Output is: ", *output)
+	for _, file := range zipReader.File {
+		fmt.Println(file.Name)
+	}
+
+	err = zipReader.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
