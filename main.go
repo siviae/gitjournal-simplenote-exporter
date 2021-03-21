99e0c4b6003289a13ec50675c69ca46b360fa886
Ilya Isaev <iisaev@okko.tv>
2021-03-21 18:04:57 +0300

    playing around further

--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
 	"io"
 	"log"
 	"os"
+	"path"
+	"strings"
 )
 
 func doMain(input *string, output *string) {
@@ -30,13 +32,54 @@ func doMain(input *string, output *string) {
 	}
 }
 
-func readActiveNote(value []byte, dataType jsonparser.ValueType, offset int, err error) {
-	fmt.Println(jsonparser.GetString(value, "content"))
+func processActiveNote(output *string) func(value []byte, dataType jsonparser.ValueType, offset int, err error) {
+	return func(value []byte, dataType jsonparser.ValueType, offset int, err error) {
+		content, err := jsonparser.GetString(value, "content")
+		creationDate, err := jsonparser.GetString(value, "creationDate")
+		lastModified, err := jsonparser.GetString(value, "lastModified")
+		writeFile(output, &content, &creationDate, &lastModified)
+	}
 }
 
-func readTrashedNote(value []byte, dataType jsonparser.ValueType, offset int, err error) {
-	content, err := jsonparser.GetString(value, "content")
-	fmt.Println("Trashed: ", content)
+func writeFile(folder *string, content *string, creationDate *string, lastModified *string) {
+	filename, done := extractFileName(content)
+	if done {
+		return
+	}
+
+	file, err := os.Create(path.Join(*folder, filename+".md"))
+	if err != nil {
+		fmt.Println("Unable to read source/notes.json from archive")
+		log.Fatal(err)
+	}
+	defer file.Close()
+	fmt.Fprintln(file, "---")
+	fmt.Fprintln(file, "created: ", creationDate)
+	fmt.Fprintln(file, "modified: ", lastModified)
+	fmt.Fprintln(file, "---")
+	fmt.Fprint(file, content)
+}
+
+func extractFileName(content *string) (string, bool) {
+	newLinePos := strings.Index(*content, "\n")
+	if newLinePos == -1 {
+		newLinePos = len(*content)
+	}
+	if newLinePos > 64 {
+		newLinePos = 64
+	}
+	if newLinePos == 0 {
+		return "", true
+	}
+	filename := strings.Replace((*content)[:newLinePos], string(os.PathSeparator), "", -1)
+	return filename, false
+}
+
+func processTrashedNote(output *string) func(value []byte, dataType jsonparser.ValueType, offset int, err error) {
+	return func(value []byte, dataType jsonparser.ValueType, offset int, err error) {
+		content, err := jsonparser.GetString(value, "content")
+		fmt.Println("Trashed: ", content)
+	}
 }
 
 func processNotes(output *string, file *zip.File) {
@@ -51,11 +94,11 @@ func processNotes(output *string, file *zip.File) {
 		fmt.Println("Unable to read source/notes.json from archive")
 		log.Fatal(err)
 	}
-	_, err = jsonparser.ArrayEach(json, readActiveNote, "activeNotes")
+	_, err = jsonparser.ArrayEach(json, processActiveNote(output), "activeNotes")
 	if err != nil {
 		log.Fatal(err)
 	}
-	_, err = jsonparser.ArrayEach(json, readTrashedNote, "trashedNotes")
+	_, err = jsonparser.ArrayEach(json, processTrashedNote(output), "trashedNotes")
 	if err != nil {
 		log.Fatal(err)
 	}
